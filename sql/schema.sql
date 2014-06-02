8b213cf8498e1aa24d8f821890606867005eb2b1
Elias Norberg <xyzzy@kudzu.se>
2014-06-02 10:58:44 +0200

    updated schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,9 +1,12 @@
 BEGIN TRANSACTION;
-CREATE TABLE customer (id INTEGER PRIMARY KEY, name TEXT, reference TEXT, postcode TEXT, city TEXT, address1 TEXT, address2 TEXT);
-INSERT INTO customer VALUES(1,'Standardkund','-',NULL,NULL,NULL,NULL);
-CREATE TABLE order_products (price NUMERIC, orderId INTEGER, productId INTEGER, comment TEXT, count NUMERIC);
-CREATE TABLE orders (id INTEGER PRIMARY KEY, description TEXT, customerId INTEGER, status INTEGER, invoiceFile TEXT, created datetime);
-INSERT INTO orders VALUES(NULL,1,1,NULL,NULL,NULL);
-CREATE TABLE products (id INTEGER PRIMARY KEY,description TEXT, physical_product NUMERIC, distributor_price money, price money, stock NUMERIC,  name TEXT, distributor NUMERIC, UPC TEXT);
-CREATE TABLE stock (productId NUMERIC, stock NUMERIC);
+CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY, name TEXT, reference TEXT, postcode TEXT, city TEXT, address1 TEXT, address2 TEXT);
+INSERT OR IGNORE INTO customer VALUES(1,'Standardkund','-',NULL,NULL,NULL,NULL);
+CREATE TABLE IF NOT EXISTS order_products (id INTEGER PRIMARY KEY, price NUMERIC, orderId INTEGER, productId INTEGER, comment TEXT, count NUMERIC);
+CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, description TEXT, customerId INTEGER, status INTEGER, invoiceFile TEXT, created datetime);
+CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,description TEXT, physical_product INTEGER, distributor_price money, price money, stock NUMERIC,  name TEXT, distributor NUMERIC, UPC TEXT);
+CREATE TABLE IF NOT EXISTS stock (productId NUMERIC, stock NUMERIC);
+CREATE TABLE IF NOT EXISTS distributor (id INTEGER PRIMARY KEY, name TEXT);
+INSERT OR IGNORE INTO distributor VALUES(1,'Skeppshult');
+INSERT OR IGNORE INTO distributor VALUES(2,'Shimano');
+
 COMMIT;
