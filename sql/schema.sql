54c5be8497abe66e936b17810bd6aa226db1d264
Elias Norberg <xyzzy@kudzu.se>
2014-06-04 00:07:31 +0200

    Update schema to use DECIMAL as column type

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,9 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY, name TEXT, reference TEXT, postcode TEXT, city TEXT, address1 TEXT, address2 TEXT);
 INSERT OR IGNORE INTO customer VALUES(1,'Standardkund','-',NULL,NULL,NULL,NULL);
-CREATE TABLE IF NOT EXISTS order_products (id INTEGER PRIMARY KEY, price NUMERIC, orderId INTEGER, productId INTEGER, comment TEXT, count NUMERIC);
+CREATE TABLE IF NOT EXISTS order_products (id INTEGER PRIMARY KEY, price DECIMAL, orderId INTEGER, productId INTEGER, comment TEXT, count DECIMAL);
 CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, description TEXT, customerId INTEGER, status INTEGER, invoiceFile TEXT, created datetime);
-CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY,description TEXT, physical_product INTEGER, distributor_price money, price money, stock NUMERIC,  name TEXT, distributor NUMERIC, UPC TEXT);
-CREATE TABLE IF NOT EXISTS stock (productId NUMERIC, stock NUMERIC);
+CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, description TEXT, physical_product INTEGER, distributor_price DECIMAL, price DECIMAL, stock DECIMAL,  distributor INTEGER, UPC TEXT);
 CREATE TABLE IF NOT EXISTS distributor (id INTEGER PRIMARY KEY, name TEXT);
 INSERT OR IGNORE INTO distributor VALUES(1,'Skeppshult');
 INSERT OR IGNORE INTO distributor VALUES(2,'Shimano');
